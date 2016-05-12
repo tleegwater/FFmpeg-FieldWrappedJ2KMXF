e252717ad1d59cc1aa7284ae1de5830c335a422f
Thijs Leegwater <t.leegwater@picturae.nl>
2016-05-12 11:43:41 +0200

    write metadata in footer

--- libavformat/mxfenc.c
+++ libavformat/mxfenc.c
@@ -1408,6 +1408,43 @@ static int mxf_write_header_metadata_sets(AVFormatContext *s)
     return 0;
 }
 
+static int mxf_write_footer_metadata_sets(AVFormatContext *s)
+{
+    const char *material_package_name = NULL;
+    const char *file_package_name = NULL;
+    AVDictionaryEntry *entry = NULL;
+    AVStream *st = NULL;
+    int i;
+
+    if (entry = av_dict_get(s->metadata, "material_package_name", NULL, 0))
+       material_package_name = entry->value;
+
+    if (entry = av_dict_get(s->metadata, "file_package_name", NULL, 0)) {
+        file_package_name = entry->value;
+    } else {
+        /* check if any of the streams contain a file_package_name */
+        for (i = 0; i < s->nb_streams; i++) {
+            st = s->streams[i];
+            if (entry = av_dict_get(st->metadata, "file_package_name", NULL, 0)) {
+                file_package_name = entry->value;
+                break;
+            }
+        }
+    }
+
+    mxf_write_preface(s);
+    mxf_write_content_storage(s);
+    mxf_write_essence_container_data(s);
+    mxf_write_package(s, MaterialPackage, material_package_name);
+    mxf_write_package(s, SourcePackage, file_package_name);
+    mxf_write_identification(s);
+  
+    
+    return 0;
+}
+
+
+
 static unsigned klv_fill_size(uint64_t size)
 {
     unsigned pad = KAG_SIZE - (size & (KAG_SIZE-1));
